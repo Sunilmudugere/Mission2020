19549a08a3a2f227f07de442cf16ab02c17ee724
M P Sunil <sunil.mp@tavant.com>
2019-01-02 12:57:02 +0530

    Initial Commit

---
+++ newProj.API/Data/DataContext.cs
@@ -0,0 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using newProj.API.Models;
+
+namespace newProj.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options): base(options){}
+        public DbSet<Value> Values{get;set;}
+    }
+}
